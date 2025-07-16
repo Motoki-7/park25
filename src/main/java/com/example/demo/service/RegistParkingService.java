1d7fd9dcfafe2901bcafafb9f38fc51de97597b1
vitamin9279 <username@users.noreply.github.com>
2025-07-16 16:13:29 +0900

    登録・編集Controllerのアノテーション修正

--- src/main/java/com/example/demo/service/RegistParkingService.java
+++ src/main/java/com/example/demo/service/RegistParkingService.java
@@ -1,11 +1,13 @@
 package com.example.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.dao.ParkinglotDao;
 import com.example.demo.entity.ParkinglotEntity;
 import com.example.demo.form.RegistParkingForm;
 
+@Service
 public class RegistParkingService {
 	@Autowired
 	ParkinglotDao dao;
