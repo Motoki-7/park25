ded84146fcb1f3621e61bac16e0ad345d121fd0a
vitamin9279 <funya@gmail.com>
2025-07-16 15:09:52 +0900

    バックエンド：編集・削除関連ファイル

--- src/main/java/com/example/demo/controller/EditParkingController.java
+++ src/main/java/com/example/demo/controller/EditParkingController.java
@@ -1,5 +1,24 @@
 package com.example.demo.controller;
 
-public class EditParkingController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.form.EditParkingForm;
+import com.example.demo.service.EditParkingService;
+
+@Controller
+public class EditParkingController {
+	@Autowired
+	EditParkingService service;
+	
+
+//	編集の確認とデータの更新
+	//	ソースは仮
+	@PostMapping("/EditParking/put")
+	public String update(Model model, EditParkingForm form) {
+		service.update(form);
+		return "/HomeAdmin";
+	}
 }
--- src/main/java/com/example/demo/form/RegistParkingForm.java
+++ src/main/java/com/example/demo/form/RegistParkingForm.java
@@ -1,5 +1,37 @@
 package com.example.demo.form;
 
-public class RegistParkingForm {
+import java.time.LocalDate;
 
+import com.example.demo.entity.ParkinglotEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegistParkingForm {
+	private int id;
+	private String address1;
+	private String address2;
+	private String address3;
+	private String name;
+	private int capacity;
+	private int hourlyRate;
+	private  LocalDate updateDate;
+	
+	public ParkinglotEntity toEntity() {
+		ParkinglotEntity ent = new ParkinglotEntity();
+//		Regist は id がAUTO_INCREMENT のため View で含めずに Controller へ
+//		ent.setId(this.id);
+		ent.setAddress1(this.address1);
+		ent.setAddress2(this.address2);
+		ent.setAddress3(this.address3);
+		ent.setName(this.name);
+		ent.setCapacity(this.capacity);
+		ent.setHourlyRate(this.hourlyRate);
+		ent.setUpdateDate(this.updateDate);
+		return ent;
+	}
 }
