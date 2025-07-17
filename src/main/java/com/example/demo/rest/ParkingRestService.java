56942fccccf042246576b828876c6e126d75d298
vitamin9279 <username@users.noreply.github.com>
2025-07-17 15:51:29 +0900

    管理者ホーム画面での検索処理null受付を修正、HTMLのjQueryソース文の修正：ParkingRestController, ParkingRestService, *.html

--- src/main/java/com/example/demo/rest/ParkingRestService.java
+++ src/main/java/com/example/demo/rest/ParkingRestService.java
@@ -17,17 +17,19 @@ public class ParkingRestService {
 	//条件検索
 	public List<ParkinglotEntity> select(String name, String address1){
 		List<ParkinglotEntity> parkinglotList;
+		boolean isNameEmpty = (name == null || name.isEmpty());
+	    boolean isAddressEmpty = (address1 == null || address1.isEmpty());
 		
 		//両方空欄の場合に全件表示
-		if(name == "" && address1 == "") {
+		if(isNameEmpty && isAddressEmpty) {
 			parkinglotList = parkinglotDao.selectAll();
 		}	
 		//nameが空欄で、address1が入力された場合　→　address1で検索
-		else if(name == "" && address1 != "") {
+		else if(isNameEmpty && !isAddressEmpty) {
 			parkinglotList = parkinglotDao.selectByAddress1(address1);
 		}
 		//address1が空欄で、nameが入力された場合　→　nameで検索
-		else if(name != "" && address1 == "") {
+		else if(!isNameEmpty && isAddressEmpty) {
 			parkinglotList = parkinglotDao.selectByName(name);
 		}
 		//両方が入力された場合　→　両方で検索
