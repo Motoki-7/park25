01401e20aaa256c7e51b2105200bb04e78898603
vitamin9279 <username@users.noreply.github.com>
2025-07-22 16:44:12 +0900

    本番環境用にテーブル定義を変更：schema.sql, data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,8 @@
-INSERT INTO parkinglot(address1,address2,address3,name,capacity,hourlyRate,updateDate) values ('東京都','品川区西五反田','1丁目','タイムズパーク24グループ本社ビル',50,1320,'2025-07-20');
+--本番環境用--------------------------
+--INSERT IGNORE INTO parkinglot(id,address1,address2,address3,name,capacity,hourlyRate,updateDate) 
+--VALUES (1,'東京都','品川区西五反田','1丁目','タイムズパーク24グループ本社ビル',50,1320,'2025-07-20');
+
+
+--ローカルテスト用--------------------------
+INSERT INTO parkinglot(address1,address2,address3,name,capacity,hourlyRate,updateDate) 
+VALUES ('東京都','品川区西五反田','1丁目','タイムズパーク24グループ本社ビル',50,1320,'2025-07-20');
