9929e5169093caa215c276ec799a503f3b24db29
Motoki-7 <username@users.noreply.github.com>
2025-07-24 14:09:01 +0900

    データベースとentityを追加：RangeEntity.java,RatesEntity.java,datasql,schema.sql

---
+++ src/main/java/com/example/demo/entity/RangeEntity.java
@@ -0,0 +1,15 @@
+package com.example.demo.entity;
+
+public class RangeEntity {
+	private int rangeId;
+	private int startTime;
+	private int endTime;
+	private boolean monday;
+	private boolean tuesday;
+	private boolean wednesday;
+	private boolean thursday;
+	private boolean friday;
+	private boolean saturday;
+	private boolean sunday;
+	private boolean holiday;
+}
