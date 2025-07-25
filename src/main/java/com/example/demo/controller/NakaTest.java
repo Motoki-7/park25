aa8db23ae506a48029f2aa481858eb81023255f6
Motoki-7 <username@users.noreply.github.com>
2025-07-25 10:04:45 +0900

    新規追加ページ編集、シミュレーション画面作成

--- src/main/java/com/example/demo/controller/NakaTest.java
+++ src/main/java/com/example/demo/controller/NakaTest.java
@@ -1,13 +1,18 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class NakaTest {
-	@GetMapping("/nakatest")
+	@PostMapping("/nakatest")
 	public String nakaTest() {
 		return "naka_test";
+		
+	}
+	@PostMapping("/nakatest2")
+	public String nakaTest2() {
+		return "naka_test2";
 		
 	}
 }
