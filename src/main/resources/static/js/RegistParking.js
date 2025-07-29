4817570d1632b07529e39ffeede83da2eb3537e9
Motoki-7 <username@users.noreply.github.com>
2025-07-29 16:55:15 +0900

    cssの修正：RegistParking2.css,RegistParking.js,EdiParking.html,RegistParking.html

--- src/main/resources/static/js/RegistParking.js
+++ src/main/resources/static/js/RegistParking.js
@@ -122,20 +122,25 @@ function addBaseFee(index) {
 							    <td><input class="id_basefee1" type="checkbox" name="dailyList[${index}].holiday" value="true"></td>
 							    <td><button type="button" class="id_basefee1 deleteBaseFeeBtn">削除</button></td>
 							  </tr>
-					        </table>
+					        </table><br>
 					        <div>
-								<label>開始時刻:</label>
-								<select class="id_basefee1 hour-select" name="dailyList[${index}].startTime"></select>
-								<label>時</label>
-								<label>　終了時刻:</label>
-								<select class="id_basefee1 hour-select" name="dailyList[${index}].endTime"></select>
-								<label>時</label><br>
-					          <input type="number" class="id_basefee1" name="dailyList[${index}].time" min="0" required>
-					          <label>分</label>
-					          <input type="number" class="id_basefee1" name="dailyList[${index}].amount" min="0" required>
-					          <label>円</label>
-					          <input type="number" class="id_basefee1" name="dailyList[${index}].maxRateTimely" min="0">
-					          <label>最大料金 円</label>
+								<div class="time_selecter">
+									<span>開始時刻:</span>
+									<select class="id_basefee1 hour-select" name="dailyList[${index}].startTime"></select>
+									<span>時</span>
+									<span>　終了時刻:</span>
+									<select class="id_basefee1 hour-select" name="dailyList[${index}].endTime"></select>
+									<span>時</span>
+								</div>
+								<div>
+						          <input type="number" class="id_basefee1" name="dailyList[${index}].time" min="0" required>
+						          <span>分</span>
+						          <input type="number" class="id_basefee1" name="dailyList[${index}].amount" min="0" required>
+						          <span>円</span>
+								  <span>最大</span>
+						          <input type="number" class="id_basefee1" name="dailyList[${index}].maxRateTimely" min="0">
+						          <span>円</span>
+								 </div>
 					        </div>
 					      </div>
 					    `);
@@ -167,21 +172,21 @@ function addBaseFee(index) {
 
 function rangeValidation() {
 	const rows = document.getElementsByClassName("baseFeeBlock");
-	let array724 = []; 
+	let array724 = [];
 	for (let i = 0; i < 7; i++) {
-	  const row = [];
-	  for (let j = 0; j < 24; j++) {
-	    row.push(false);
-	  }
-	  array724.push(row);
+		const row = [];
+		for (let j = 0; j < 24; j++) {
+			row.push(false);
+		}
+		array724.push(row);
 	}
-	let array724error = []; 
+	let array724error = [];
 	for (let i = 0; i < 7; i++) {
-	  const row = [];
-	  for (let j = 0; j < 24; j++) {
-	    row.push(false);
-	  }
-	  array724error.push(row);
+		const row = [];
+		for (let j = 0; j < 24; j++) {
+			row.push(false);
+		}
+		array724error.push(row);
 	}
 	const days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
 	const daysJ = ["月曜", "火曜", "水曜", "木曜", "金曜", "土曜", "日曜"];
@@ -191,30 +196,30 @@ function rangeValidation() {
 		const endTime = row.querySelector('select[name$=".endTime"]');
 		let startTimeValue = Number(startTime?.value);
 		let endTimeValue = Number(endTime?.value);
-		if(startTimeValue >= endTimeValue){
+		if (startTimeValue >= endTimeValue) {
 			endTimeValue += 24;
 		}
 		days.forEach((day, dayIndex) => {
 			const checkbox = row.querySelector(`input[name$=".${day}"]`);
-			if(checkbox?.checked){
-				for(let j = startTimeValue; j < endTimeValue; j++){
-					if(j < 24){
-						if(array724[dayIndex][j]){
+			if (checkbox?.checked) {
+				for (let j = startTimeValue; j < endTimeValue; j++) {
+					if (j < 24) {
+						if (array724[dayIndex][j]) {
 							array724error[dayIndex][j] = true;
-						}else{
+						} else {
 							array724[dayIndex][j] = true;
 						}
-					} else{
+					} else {
 						let dayIndexbuf = dayIndex + 1;
-						if(dayIndexbuf > 6){
+						if (dayIndexbuf > 6) {
 							dayIndexbuf = 0;
 						}
-						if(array724[dayIndexbuf][j - 24]){
+						if (array724[dayIndexbuf][j - 24]) {
 							array724error[dayIndexbuf][j - 24] = true;
-						}else{
+						} else {
 							array724[dayIndexbuf][j - 24] = true;
 						}
-					}		
+					}
 				}
 			}
 		});
@@ -223,56 +228,56 @@ function rangeValidation() {
 	daysJ.forEach((day, dayIndex) => {
 		let start = null;
 		for (let i = 0; i < array724error[dayIndex].length; i++) {
-		  if (array724error[dayIndex][i]) {
-		    if (start === null) {
-		      start = i;
-		    }
-		  } else {
-		    if (start !== null) {
-		        result.push(`${day} ${start}時から${i}時`);
-		      start = null;
-		    }
-		  }
+			if (array724error[dayIndex][i]) {
+				if (start === null) {
+					start = i;
+				}
+			} else {
+				if (start !== null) {
+					result.push(`${day} ${start}時から${i}時`);
+					start = null;
+				}
+			}
 		}
 
 		// 最後が true で終わる場合の処理
 		if (start !== null) {
-		    result.push(`${day} ${start}時から${array724error[dayIndex].length - 24}時`);
+			result.push(`${day} ${start}時から${array724error[dayIndex].length - 24}時`);
 		}
 	});
 	if (result.length !== 0) {
-	  result.unshift(`期間が重複しています。`);
+		result.unshift(`期間が重複しています。`);
 	}
-	
+
 	let result2 = [];
 	daysJ.forEach((day, dayIndex) => {
 		let start = null;
 		for (let i = 0; i < array724[dayIndex].length; i++) {
-		  if (!array724[dayIndex][i]) {
-		    if (start === null) {
-		      start = i;
-		    }
-		  } else {
-		    if (start !== null) {
-		        result2.push(`${day} ${start}時から${i}時`);
-		      start = null;
-		    }
-		  }
+			if (!array724[dayIndex][i]) {
+				if (start === null) {
+					start = i;
+				}
+			} else {
+				if (start !== null) {
+					result2.push(`${day} ${start}時から${i}時`);
+					start = null;
+				}
+			}
 		}
 
 		// 最後が true で終わる場合の処理
 		if (start !== null) {
-		    result2.push(`${day} ${start}時から${array724[dayIndex].length - 24}時`);
+			result2.push(`${day} ${start}時から${array724[dayIndex].length - 24}時`);
 		}
 	});
 	if (result2.length !== 0) {
-	  result2.unshift(`未設定の期間があります。`);
+		result2.unshift(`未設定の期間があります。`);
 	}
 	const outputDivUsed = document.getElementById("outputRangeValidationUsed");
 	const outputDivNotSet = document.getElementById("outputRangeValidationNotSet");
 	outputDivUsed.innerHTML = result.map(line => `<div>${line}</div>`).join("");
 	outputDivNotSet.innerHTML = result2.map(line => `<div>${line}</div>`).join("");
-	if(result.length == 0 && result2.length == 0){
+	if (result.length == 0 && result2.length == 0) {
 		return true;
 	}
 	return false;
